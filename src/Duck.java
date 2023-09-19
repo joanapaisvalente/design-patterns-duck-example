1f358a84d5a17104eab1893aa3428167c4314979
Joana Valente <108897611+joanapaisvalente@users.noreply.github.com>
2023-09-19 17:31:28 +0100

    setting duck behaviour dinamically

--- src/Duck.java
+++ src/Duck.java
@@ -6,11 +6,21 @@ public abstract class Duck {
 
     }
 
+    public void display() {}
+
     public void performFly() {
         flyBehaviour.fly();
     }
 
     public void performQuack() {
         quackBehaviour.quack();
+    }
+
+    public void setFlyBehaviour(FlyBehaviour flyBehaviour) {
+        this.flyBehaviour = flyBehaviour;
+    }
+
+    public void setQuackBehaviour(QuackBehaviour quackBehaviour) {
+        this.quackBehaviour = quackBehaviour;
     }
 }
--- src/main.java
+++ src/main.java
@@ -1,5 +1,14 @@
 public class main {
     public static void main(String[] args) {
+        makeMallardDuckPerform();
+
+        Duck model = new ModelDuck();
+        model.performFly();
+        model.setFlyBehaviour(new FlyRocketPowered());
+        model.performFly();
+    }
+
+    private static void makeMallardDuckPerform() {
         Duck mallardDuck = new MallardDuck();
         mallardDuck.performFly();
         mallardDuck.performQuack();
